21b340ac50b62b92a630d10ea71112cc69d9f09e
卓泽 <wisteria_villosa_rehder@outlook.com>
2024-01-27 17:23:08 +0800

    ui_complated

---
+++ Assets/UI/ManagerUI.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseUI : MonoBehaviour
+    {
+    public Button pause_button;
+    public Button continue_button;
+    public void PauseGame()
+        {
+        Time.timeScale = 1;
+        }
+    public void ContinueGame()
+        {
+        Time.timeScale = 0;
+        }
+
+    public void debug_test1()
+    {
+        print("1 test button hello world!");
+    }
+
+    private void Start()
+        {
+        pause_button.onClick.AddListener(PauseGame);
+        continue_button.onClick.AddListener(ContinueGame);
+        }
+    }
