e2fd1047c97b38a3b84089db2634ac5edce87b86
NotWhiskey12138 <137181834+NotWhiskey12138@users.noreply.github.com>
2024-01-28 13:44:11 +0800

    结算

--- Assets/Scripts/General/WinnerReport.cs
+++ Assets/Scripts/General/WinnerReport.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,6 +7,25 @@ using UnityEngine;
 public class WinnerReport : MonoSingleton<WinnerReport>
 {
     public bool ifP1won;
+
+    public GameObject pannal1_winner;
+    public GameObject pannal2_winner;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        string crs = col.ToString();
+        if (col.CompareTag("Player") && col.gameObject.ToString()=="player1 (UnityEngine.GameObject)")
+        {
+            Debug.Log("Player1 is win");
+            pannal1_winner.SetActive(true);
+        }
+        else if (col.CompareTag("Player") && col.gameObject.ToString() == "player2 (UnityEngine.GameObject)")
+        {
+            Debug.Log("Player2 is win");
+            pannal2_winner.SetActive(true);
+        }
+    }
+
     public void checkWinner()
     {
         if (PlayerController.Instance.getIsStage())
